357345a989c853961b7676ac1f702a8a6749fc15
Srujan Jha <srujan.jha@capillarytech.com>
2017-02-09 14:26:52 +0530

    Change3.0

--- Commit.py
+++ Commit.py
@@ -1,3 +1,12 @@
+import getopt,sys
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    if args[0] == "refs/heads/master":
+        if not (args[1] == "refs/heads/nightly" or args[1] == "refs/heads/staging" or args[1] == "refs/heads/production"):
+            sys.exit(1)
+except getopt.GetoptError as err:
+    print str(err) # will print something like "option -a not recognized"
+    sys.exit(2)
 version="patch"
 while 1:
     version = raw_input("Enter the version to be bumped(major/minor/patch)[default:patch]:")
